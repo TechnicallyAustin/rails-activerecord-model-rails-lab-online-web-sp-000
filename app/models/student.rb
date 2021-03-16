a2877e35e71de476e33b600da6031672cdf737f7
Austin Johnson <technicallyaustin@gmail.com>
2021-03-16 18:12:13 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,12 @@
 class Student < ActiveRecord::Base 
+<<<<<<< HEAD
     def to_s
         p "#{first_name}" + " " + "#{last_name}"
     end
 end
+=======
+  def to_s 
+    p "#{first_name}" + " " + "#{last_name}"
+  end
+end
+>>>>>>> ba3e924069cd46c68e21eb5aad7598eca7450797
